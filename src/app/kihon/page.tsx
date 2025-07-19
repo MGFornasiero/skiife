999f9f7e0ef7773b86123ae02eb0c259422d9e66
Marco Fornasiero <wrk.fornasiero@gmail.com>
2025-07-19 09:55:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="flex f

--- src/app/kihon/page.tsx
+++ src/app/kihon/page.tsx
@@ -1,16 +1,9 @@
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import KataDisplay from "@/components/kata-display";
 
 export default function KihonPage() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Kihon</CardTitle>
-        <CardDescription>Select a grade to see its detailed kihon sequenze.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <KataDisplay />
-      </CardContent>
-    </Card>
+    <div className="w-full">
+      <KataDisplay />
+    </div>
   );
 }
