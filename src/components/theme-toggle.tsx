3d07c223090708e6081b47a368229fdd50a24474
Marco Fornasiero <wrk.fornasiero@gmail.com>
2025-07-21 12:51:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent>
    
    wit

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,40 +1,47 @@
 "use client"
 
 import * as React from "react"
-import { Moon, Sun } from "lucide-react"
+import { Moon, Sun, Monitor } from "lucide-react"
 
 import { useTheme } from "./theme-provider"
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip"
+
+const themeIcons = {
+  light: <Sun className="h-[1.2rem] w-[1.2rem]" />,
+  dark: <Moon className="h-[1.2rem] w-[1.2rem]" />,
+  system: <Monitor className="h-[1.2rem] w-[1.2rem]" />,
+}
+
+const themeOrder: ("light" | "dark" | "system")[] = ["light", "dark", "system"];
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  const handleToggle = () => {
+    const currentIndex = themeOrder.indexOf(theme);
+    const nextIndex = (currentIndex + 1) % themeOrder.length;
+    setTheme(themeOrder[nextIndex]);
+  };
+
+  const currentIcon = themeIcons[theme] || themeIcons.system;
+  const capitalizedTheme = theme.charAt(0).toUpperCase() + theme.slice(1);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline" size="icon" onClick={handleToggle}>
+            {currentIcon}
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Theme: {capitalizedTheme}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
+
+    
